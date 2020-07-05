4f43339bd7acf37bb7e907881e5b312464a11dc9
Or <66566274+Or-Katzir@users.noreply.github.com>
2020-07-05 23:16:58 +0300

    Update Line.java
    1. Adding new documentation to the code 
    2. removing unnecessary methods

--- src/Line.java
+++ src/Line.java
@@ -1,25 +1,32 @@
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.io.Serializable;
 
+/*
+ * <h2>Class for creating a line object to draw to the screen</h2>
+ * 
+ * @author Or Katzir
+ * 
+ * @version 2
+ */
 public class Line extends Shapes {
 
 	private Point p1,p2;
-	private Color c=new Color(0,0,0);
 	
+	/*
+	 * <h1>Constructor to create a line object</h1>
+	 * @param p1 A point object for drawing the line
+	 * @param p2 A point object for drawing the line
+	 */
 	public Line(Point p1, Point p2) {
 		this.p1=p1;
 		this.p2=p2;
 	}
-	
-	public void setColor(Color c) {
-		this.c=c;
-	}
-	
-	public void draw(Graphics g) {
-		
-		g.setColor(c);
+
+	/*
+	 * Method to draw the line to screen 
+	 */
+	public void draw(Graphics g) {	
+		g.setColor(color);
 		g.drawLine((int)p1.getX(), (int)p1.getY(), (int)p2.getX(), (int)p2.getY());
 
 	}
