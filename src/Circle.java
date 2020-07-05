0e9906071e12a72cded2fc839f9649407b559881
Or <66566274+Or-Katzir@users.noreply.github.com>
2020-07-05 22:37:39 +0300

    Update Circle.java
    1. Adding new documentation to the code
    2. Change the constructor to take two Point object for setting the size and location of the circle 
    3. Change the constructor set the fill option and the color
    4. removing unnecessary method

--- src/Circle.java
+++ src/Circle.java
@@ -1,39 +1,43 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 
-
+/*
+ * <h2>Class to implements a circle object</h2> 
+ * @author Or Katir
+ * @version 2 
+ */
 public class Circle extends Shapes{
 	
-	private int x,y;
-	private boolean fill;
-	private Color  c=new Color(0,0,0);
+	private Point p1;
+	private int width, height; //The width and height of the circle 
 	
-	public Circle(int x, int y) {
-		this.x=x;
-		this.y=y;
-
+	/*
+	 * <h1>Constructor to create a circle object</h1>
+	 * @param p1 A point object for drawing the upper left corner of the rectangle that surround the circle
+	 * @param p2 A point object for calculating the width and height of the circle
+	 * @param color The color of the circle 
+	 * @param fill boolean variable to set the filling option for the circle 
+	 */
+	public Circle(Point p1, Point p2, Color color, boolean fill) {
+		this.p1 = p1;
+		width = Math.abs((int)p1.getX() - (int)p2.getX());
+		height = Math.abs((int)p1.getY() - (int)p2.getY());
+		this.color = color;
+		this.fill = fill;
 	}
 	
-	public void setColor(Color c) {
-		this.c=c;
-	}
-	
-	
+	/*
+	 * Method to draw the circle object to the drawing panel 
+	 * @param fill If true the circle will be filled with color
+	 */
 	public void draw(Graphics g) {
-		g.setColor(c);
+		g.setColor(color);
 		
 		if(fill)
-			g.fillOval(x-20, y-20, 40, 40);
+			g.fillOval((int)p1.getX(), (int)p1.getY(), width, height);
 		
-		g.drawOval(x-20, y-20, 40, 40);
-	}
-
-	public boolean isFill() {
-		return fill;
-	}
-
-	public void setFill(boolean fill) {
-		this.fill = fill;
+		g.drawOval((int)p1.getX(), (int)p1.getY(), width, height);
 	}
 
 }
