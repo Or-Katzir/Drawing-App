288264a139017316fc38e18f7340890145ebfe99
Or <66566274+Or-Katzir@users.noreply.github.com>
2020-07-05 23:26:52 +0300

    Update Line3.java
    1. Adding new documentation to the code 
    2. removing unnecessary methods

--- src/Line3.java
+++ src/Line3.java
@@ -1,25 +1,37 @@
 
-import java.awt.Color;
+
 import java.awt.Graphics;
 import java.awt.Point;
 
+
+/*
+ * <h2>Class for creating a line object to draw to the screen</h2>
+ * This will be the thickest line in the application
+ * @author Or Katzir
+ * 
+ * @version 2
+ */
 public class Line3 extends Shapes{
 
 	private Point p1;
-	private Color c=new Color(0,0,0);
 	
+
+	/*
+	 * <h1>Constructor to create a line object</h1>
+	 * @param p1 A point object for drawing the line
+	 * @param p2 A point object for drawing the line
+	 */
 	public Line3(Point p1, Point p2) {
 		this.p1=p1;
 	}
 	
-	public void setColor(Color c) {
-		this.c=c;
-	}
-	
-	
+	/*
+	 * Method to draw the line to screen 
+	 * The line will be drawn using oval objects
+	 */
 	public void draw(Graphics g) {
 		
-		g.setColor(c);
+		g.setColor(color);
 		g.fillOval((int)p1.getX(), (int)p1.getY(), 35, 35);
 		
 	}
